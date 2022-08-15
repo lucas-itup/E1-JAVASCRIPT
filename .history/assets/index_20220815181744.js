2ff80c7892e95bc8e2090e2a3b192e5b712e305f
Lucas25 <lucas@it-up.com.ar>
2022-08-15 19:06:49 -0300

    first commit

---
+++ .history/assets/index_20220815181744.js
@@ -0,0 +1,64 @@
+let pizza1 = {
+    id: '1',
+    nombre: 'Muzarella',
+    ingredientes: [
+        'tomate', 'muzarella', 'oregano'
+    ],
+    precio: '600'
+}
+let pizza2 = {
+    id: '2',
+    nombre: 'Napolitana',
+    ingredientes: [
+        'jamon', 'huevo', 'queso'
+    ],
+    precio: '300'
+}
+let pizza3 = {
+    id: '3',
+    nombre: 'Americana',
+    ingredientes: [
+        'huevoFrito', 'peperoni', 'roquefort'
+    ],
+    precio: '200'
+}
+let pizza4 = {
+    id: '4',
+    nombre: 'Rucula',
+    ingredientes: [
+        'verduras', 'palmito', 'especias'
+    ],
+    precio: '500'
+}
+let pizza5 = {
+    id: '5',
+    nombre: 'Especial',
+    ingredientes: [
+        'salsa', 'harina', 'crema'
+    ],
+    precio: '800'
+}
+let pizza6 = {
+    id: '6',
+    nombre: 'DE_LA_CASA',
+    ingredientes: [
+        'anchoas', 'pimientos', 'hongos'
+    ],
+    precio: '900'
+}
+
+let pizzas = [pizza1, pizza2, pizza3, pizza4, pizza5, pizza6]
+
+pizzas.forEach(function pizza(e, i, array) {
+
+    //listado de pizzas
+    console.log('La pizza ' + e.nombre + ' tiene un precio de ' + e.precio + ' , un id de ' + e.id)
+
+
+    //pizzas con id impar
+    if (e.id % 2 == 1) console.log('Las pizzas con id impar son: ' + e.nombre)
+
+    //pizzas menores a $600
+
+    if (e.precio < "600") console.log('Las pizzas con un valor menos a $600 son: ' + e.nombre)
+})
---
+++ .history/assets/index_20220815184554.js
@@ -0,0 +1,69 @@
+let pizza1 = {
+    id: '1',
+    nombre: 'Muzarella',
+    ingredientes: [
+        'tomate', 'muzarella', 'oregano'
+    ],
+    precio: '600'
+}
+let pizza2 = {
+    id: '2',
+    nombre: 'Napolitana',
+    ingredientes: [
+        'jamon', 'huevo', 'queso'
+    ],
+    precio: '300'
+}
+let pizza3 = {
+    id: '3',
+    nombre: 'Americana',
+    ingredientes: [
+        'huevoFrito', 'peperoni', 'roquefort'
+    ],
+    precio: '200'
+}
+let pizza4 = {
+    id: '4',
+    nombre: 'Rucula',
+    ingredientes: [
+        'verduras', 'palmito', 'especias'
+    ],
+    precio: '500'
+}
+let pizza5 = {
+    id: '5',
+    nombre: 'Especial',
+    ingredientes: [
+        'salsa', 'harina', 'crema'
+    ],
+    precio: '800'
+}
+let pizza6 = {
+    id: '6',
+    nombre: 'DE_LA_CASA',
+    ingredientes: [
+        'anchoas', 'pimientos', 'hongos'
+    ],
+    precio: '900'
+}
+
+let pizzas = [pizza1, pizza2, pizza3, pizza4, pizza5, pizza6]
+let impares = [];
+let menores = [];
+
+pizzas.forEach(function pizza(e, i, array) {
+
+    //pizzas con id impar
+    if (e.id % 2 == 1) impares.push(e.nombre)
+
+    //pizzas menores a $600
+    if (e.precio < "600") menores.push(e.nombre)
+
+    console.log('La pizza ' + e.nombre + ' tiene un precio de $' + e.precio + ' , un id de ' + e.id + ' INGREDIENTES: ' + e.ingredientes.join())
+    listado.innerHTML += 'La pizza ' + e.nombre + ' tiene un precio de $' + e.precio + ' , un id de ' + e.id + ' INGREDIENTES: ' + e.ingredientes.join()
+})
+
+console.log('Las pizzas que tienen un id impar son: ' + impares.join());
+console.log('Las pizzas con un valor menor a $600 son: ' + menores.join());
+
+let listado = document.getElementById('listado-pizzas');
